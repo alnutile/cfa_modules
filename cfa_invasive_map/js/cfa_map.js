0c9228d57ffc70ac980d838ab8a8608a2b6c9f7f
andy <andy@commonmediainc.com>
2013-11-22 18:40:51 -0500

    new stuff

--- cfa_invasive_map/js/cfa_map.js
+++ cfa_invasive_map/js/cfa_map.js
@@ -1,21 +1,21 @@
 (function ($) {
-  Drupal.behaviors.cfa_map = {
-    attach: function(context) {    
-      var cfa_map = {};
-cfa_map['chicago'] = {
-  center: new google.maps.LatLng(41.878113, -87.629798),
-  population: 2842518
-};
-cfa_map['newyork'] = {
-  center: new google.maps.LatLng(40.714352, -74.005973),
-  population: 8143197
-};
-cfa_map['losangeles'] = {
-  center: new google.maps.LatLng(34.052234, -118.243684),
-  population: 3844829
-};
+  Drupal.behaviors.cfa_invasive_map = {
+    attach: function(context, settings) {    
+
+
+
+
+
+
 var cfa_mapCircle;
 function initialize() {
+
+var coord = (settings.cfa_invasive_map.coordinates);
+//console.log(coord);
+
+
+var index;
+var a = coord;
   // Create the map.
   var mapOptions = {
     zoom: 4,
@@ -25,8 +25,11 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
   // Construct the circle for each value in citymap.
-  // Note: We scale the population by a factor of 20.
-  for (var city in cfa_map) {
+   for (index = 0; index < a.length; ++index) {
+    a[index] = { 
+      center: new google.maps.LatLng(lat, lon)
+    }
+    console.log(a);
     var populationOptions = {
       strokeColor: '#FFFF01',
       strokeOpacity: 0.45,
@@ -34,13 +37,61 @@ function initialize() {
       fillColor: '#FFFF01',
       fillOpacity: 0.35,
       map: map,
-      center: cfa_map[city].center,
-      radius: cfa_map[city].population / 50
+      center: a[index].center,
+      radius: 2000000
     };
     // Add the circle for this city to the map.
     cfa_mapCircle = new google.maps.Circle(populationOptions);
   }
 }
 
+
+
 google.maps.event.addDomListener(window, 'load', initialize);   
     }}})(jQuery);
+    
+    
+    
+    
+
+
+//      var cfa_map = {};
+//cfa_map['chicago'] = {
+//  center: new google.maps.LatLng(41.878113, -87.629798),
+//  population: 16093.44
+//};
+//cfa_map['newyork'] = {
+//  center: new google.maps.LatLng(40.714352, -74.005973),
+//  population: 16093.44
+//};
+//cfa_map['losangeles'] = {
+//  center: new google.maps.LatLng(34.052234, -118.243684),
+//  population: 16093.44
+//};console.log(cfa_map);
+//var cfa_mapCircle;
+//function initialize() {
+//  // Create the map.
+//  var mapOptions = {
+//    zoom: 4,
+//    center: new google.maps.LatLng(37.09024, -95.712891),
+//    mapTypeId: google.maps.MapTypeId.TERRAIN
+//  };
+//  var map = new google.maps.Map(document.getElementById('map-canvas'),
+//      mapOptions);
+//  // Construct the circle for each value in citymap.
+//  // Note: We scale the population by a factor of 20.
+//  for (var city in cfa_map) {
+//    var populationOptions = {
+//      strokeColor: '#FFFF01',
+//      strokeOpacity: 0.45,
+//      strokeWeight: 2,
+//      fillColor: '#FFFF01',
+//      fillOpacity: 0.35,
+//      map: map,
+//      center: cfa_map[city].center,
+//      radius: cfa_map[city].population 
+//    };
+//    // Add the circle for this city to the map.
+//    cfa_mapCircle = new google.maps.Circle(populationOptions);
+//  }
+//}
